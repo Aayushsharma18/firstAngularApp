6b5002ff254b6b32f7668818950c92e0ff797cd1
Aaysh Sharma <aayushnsunny78@gmail.com>
2024-12-09 11:26:02 +0530

    added control-flow component.

--- src/app/components/control-flow/control-flow.component.ts
+++ src/app/components/control-flow/control-flow.component.ts
@@ -1,14 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control-flow',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css'
 })
 export class ControlFlowComponent {
 
   div1hide: boolean = true;
+  isChecked: boolean = true;
+  day: string = "";
+  cityList: string[] = ["Surat", "Ahmedbad", "Pune", "Jaipur", "Rajkot"];
+  selectedCity: string = "select city";
+  value: string = "";
+
 
   showDiv() {
     this.div1hide = true;
